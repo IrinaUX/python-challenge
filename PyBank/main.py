f4ad225268e87fc7606a5f84f1ef2d2702fa71e3
Irina Kim <irinahpda@gmail.com>
2020-05-17 18:38:57 -0500

    Modify the average change function to incorporate the maximum profit increase and decrease definitions with their respective dates. And results write into the output.txt file.

--- PyBank/main.py
+++ PyBank/main.py
@@ -67,15 +67,21 @@ The function takes path parameter as an input and perform the following:
 Open file in read mode.
 Use csv reader method to read the file line by line.
 Use next() method to skip the header.
-Define two variables to store current and previous values to calculate the change.
-Initialize both variables as None to use in the conditional statements.
-When values are still in their initial state, then assign the current values.
-When both values are available, then compute the difference.
-For the average computation add the following:
-Two new variables to hold the running total of changes and the row count number.
-Calculate the average and round the results before writing to the text output file.
-'''
-def averageChange(path, output_file):
+a)  Define two variables to store current and previous values to calculate the change.
+    Initialize both variables as None to use in the conditional statements.
+    When values are still in their initial state, then assign the current values.
+    When both values are available, then compute the difference.
+b)  For the average computation add the following:
+    Two new variables to hold the running total of changes and the row count number.
+    Calculate the average and round the results before writing to the text output file.
+c)  To identify the greatest profit increase and decrease with their respective dates, add the following:
+    Add two variables for the greatest increase and greatest decrease.
+    Add two more variables for their respective dates.
+    Track the maximum increase and maximum decrease using another IF condition inde the else branch of For loop.
+    Output the results into the summary table
+'''               
+
+def calculateGreatestProfitIncrease(path, output_file):
     # Define the variables for the difference calculation - num0 is previous value and num1 is the current value.
     num0 = 0 # initialize variable as None (not zero, no value assigned)
     num1 = 0 # initialize variable as None (not zero, no value assigned)
@@ -84,12 +90,16 @@ def averageChange(path, output_file):
                   # Note: the row count is 86 without the header. But changes are only computed for the 85 rows, since the first row does not have previous value.
                   # Hence, need to initialize as 0 to not take first 0 changes into the average computation.
     changes_average = 0 # Create and initialize variable for the average changes.
+    changes_greatest_increased = 0 # Create and initialize variable for the greatest profit increase.
+    changes_greatest_decreased = 0 # Create and initialize variable for the greatest profit decrease.
+    date_greatest_increase = "" # Create and initialize variable to store the date for the greatest increase for the output file.
+    date_greatest_decrease = "" # Create and initialize variable to store the date for the greatest decrease for the output file.
     with open(path, 'r') as file_input:
         csv_reader_object = csv.reader(file_input, delimiter=',')
         next(csv_reader_object)
         for value in csv_reader_object: # for each line in the csv file
             if num0 == 0: # Check the second row.
-                num0 = int(value[1])
+                num0 = int(value[1]) 
             else: # for all the other rows
                 num1 = int(value[1]) # assign value to the second number.
                 change = num1 - num0 # compute the difference.
@@ -99,17 +109,33 @@ def averageChange(path, output_file):
                 print(f"Running total in changes is ${runningTotalChanges}.")
                 row_count += 1
                 print(f"Row count is {row_count}.")
+                # Add condition to check if the changes are positive or negative for the Greatest increase and Greatest decrease identification.
+                if change > changes_greatest_increased:
+                    #print(f"Calculated change of ${change} larger than recorded greatest change of ${changes_greatest_increased}.")
+                    changes_greatest_increased = change
+                    date_greatest_increase = (value[0]) # get the date from the same row index[0]
+                    print(f"The data of greatest increase is {date_greatest_increase}.")
+                    #print(f"Updated greatest change is ${change}.")
+                elif change < changes_greatest_decreased:
+                    changes_greatest_decreased = change
+                    date_greatest_decrease = (value[0]) # get the date from the same row index[0]
+                #print(f"The greatest change is ${change}.")
         # Outside of the FOR loop, compute the total of all changes over row count:
         print(f"Row count is = {row_count} and total changes are ${runningTotalChanges}.")
         changes_average = runningTotalChanges / row_count # calculate the average changes.
         print(changes_average)
+        print(f"Greatest profit increase: {date_greatest_increase} (${changes_greatest_increased})")
+        print(f"Greatest profit decrease: {date_greatest_decrease} (${changes_greatest_decreased})")
 
         # Write the results to the text file:
         textFileOutput = open(output_file,"a+")
         textFileOutput.write(f"Average Change: ${round(changes_average, 2)} \n") # round the printed output average to 2 decimal points.
+        textFileOutput.write(f"Greatest Increase in Profits: {date_greatest_increase} (${changes_greatest_increased})\n")
+        textFileOutput.write(f"Greatest Decrease in Profits: {date_greatest_decrease} (${changes_greatest_decreased})\n")
         textFileOutput.close()
+
 
 # Call the functions to perform budget analysis:
 findTotalNumberOfMonths(path)
 findRevenueTotal(path, output_file)
-averageChange(path, output_file)
+calculateGreatestProfitIncrease(path, output_file)
