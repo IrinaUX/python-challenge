aee1566a091f8fee6bf274b63a192db034146588
Irina Kim <irinahpda@gmail.com>
2020-05-17 12:53:30 -0500

    Add initial version of the function to calculate the change in pnl column.

--- PyBank/main.py
+++ PyBank/main.py
@@ -53,7 +53,7 @@ def findRevenueTotal(path):
         for value in csv_reader_object:
             profit_list.append(int(value[1]))
         total_profit_and_loss = round(sum(profit_list))
-        print(f"Total profit and loss is ${total_profit_and_loss}")
+        print(f"Total profit and loss is ${total_profit_and_loss}.")
 
         # Append to an existing file:
         textFileOutput = open("output.txt","a+")
@@ -62,16 +62,59 @@ def findRevenueTotal(path):
 
 # Define a function to calculate the average change in revenue.
 def averageChange(path):
-    changeNegative = 0
-    changePositive = 0
+    # maxPostive = 0
+    # maxNegative = 0
+    # changeNegative = []
+    # changePositive = []
+    num0 = None
+    num1 = None
+    num2 = None
     with open(path, 'r', newline='') as file_input:
         csv_reader_object = csv.reader(file_input, delimiter=',')
-        next(csv_reader_object)
+        next(csv_reader_object) # skip the header
+        #str1 = next(csv_reader_object) # assign next row to str1 parameter
+        #print(str1)
+        #string_split = str1.index[1]
+        #print(string_split)
         for value in csv_reader_object:
-            profit_list.append(int(value[1]))
+            if num0 == None: # first value in column Profit/Losses is unknown
+                num0 = int(value[1])
+                print(f"The very first value in column pnl is ${num0}.")
+                #changeNegative.append(int(value[1]))
+                #print(f"Losses collected in one list ${changeNegative}.")
+            elif num1 == None:
+                num1 = int(value[1])
+            elif num0 is not None and num1 is not None:
+                #num1 = int(value[1])
+                #print(f"The first value in column pnl is ${num1}.")
+                change = num1 - num0
+                #num1 = next(int(value[1]))
+                print(f"Next first value is changed to $ {num1}.")
+                print(f"Pnl change is ${change}.")
+                next(csv_reader_object)
+            elif num1 is not None and num2 is not None:
+                num2 = int(value[1])
+                print(f"The second value in column pnl is ${num2}.")
+                change = num2 - num1
+                print(f"Next Pnl change is ${change}.")
+                next(csv_reader_object)
+                print(f"Next second value is changed to $ {num2}.")
+            elif num0 is not None and num1 is not None: # when two values for comparison are not absent.
+                change = num1 - num0
+                print(change)
 
+
+
+    # print(f"The first value in column pnl is ${num1}.")
+    # print(f"The next value in column pnl is ${num2}.")
+    # Compute the difference between the two numbers (previous and next):
+    
+                #changePositive.append(int(value[1]))
+                #print(f"Losses collected in one list ${changePositive}.")
+    #print(f"LOSSES collected in one list ${changeNegative}.")
+    #print(f"PROFIT collected in one list ${changePositive}.")
 
 # Call functions one by one to process the budget file
-findTotalNumberOfMonths(path)
-findRevenueTotal(path)
+# findTotalNumberOfMonths(path)
+# findRevenueTotal(path)
 averageChange(path)
