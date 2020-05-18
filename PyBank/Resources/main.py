d627cce9e340477eec62611fc5fec7d8ff7229e1
Irina Kim <irinahpda@gmail.com>
2020-05-18 13:12:48 -0500

    Add new function to find and output the candidate with the maximum votes.

--- PyBank/Resources/main.py
+++
@@ -1,8 +0,0 @@
-import os
-import csv
-file = "budget_data.csv"
-with open(file, 'r') as filehandler:
-    lines=filehandler.read()
-    print(lines)
-    print(type(lines))
-
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,22 +7,16 @@ path=os.path.join("Resources","election_data.csv")
 #output_path = "analysis"
 output_file = "analysis/output.txt"
 candidates_list = []
-    
+#rows_num = 0
 # Define a function to count total number of months.
-'''
-This function takes the path variable defined outside the function as an input.
-With provided path, open the csv file using with() method to avoid issues, when forgetting to close the file.
-Open csv file in read mode and optional parameter newline default to null.
-Open csv file as "file_input".
-Use iter() method to iterate through each row in the csv file.
-Use next() method to skip the header line.
-Use list comprehension to sum up the numbers of rows.
-Print the found votes.
-Create text file named output.txt in write mode (also create new file, if does not exist).
-Write first line.
-Write new lines.
-'''
+
+
 def find_votes_total(path):
+    '''
+    This function takes the path variable defined outside the function as an input.
+    Also uses an output_file variable to specify the path and the file name for the outputs.
+    Using list comprehension, count the total number of rows and output the results as Total Votes.
+    '''
     with open(path, 'r', newline='') as file_input: # open('output.txt', 'wb') as file_output:
         #csv_reader_object = csv.reader(file_input, delimiter=',')
         iterRows = iter(file_input)
@@ -30,30 +24,25 @@ def find_votes_total(path):
         rows_num = (sum(1 for _ in iterRows))
         print("Election Results")
         print("--------------------------")
-        print(f"Total Months: {rows_num}")
+        print(f"Total Votes: {rows_num}")
         print("--------------------------")
         # Write results to the output.txt file in analysis folder
         textFileOutput = open(output_file,"w+")
         textFileOutput.write("Election Results \n")
         textFileOutput.write("--------------------------- \n")
-        textFileOutput.write(f"Total Months: {rows_num} \n")
+        textFileOutput.write(f"Total Votes: {rows_num} \n")
         textFileOutput.write("--------------------------- \n")
         textFileOutput.close()
+    #print(rows_num)
+    return rows_num 
 
-#Define function to find the total revenue:
-'''
-The function takes path parameter as an input.
-Open file in read mode.
-Use csv reader method to read the file line by line
-Use next() method to skip the header.
-Define profit_list parameter as empty list.
-Loop through the csv object and append the value from column 2 (index[1] in the array).
-At the same time, define the value as data type integer.
-Use sum method on the profit_list to obtain the total revenue.
-At the same time, round the sum value.
-Print the statement and write into output.txt file
-'''
 def create_candidates_list(path, output_file):
+    '''
+    This auxiliary function is to create a list with all the candidates in the elections.
+    The functino opens provides csv file in read mode
+    Iterates through the csv_reader_object to check if candidate is already included in the list of candidates.
+    If candidate is not yet in the list, then add it. Otherwise, continue the loop
+    '''
     # candidates_list = [] # moved to global variables
     #candidates = collections.Counter()
     with open(path, 'r', newline='') as file_input: # open('output.txt', 'wb') as file_output:
@@ -63,107 +52,62 @@ def create_candidates_list(path, output_file):
             candidate = str(row[2])
             if candidate not in candidates_list:
                 candidates_list.append(candidate)
-        print(candidates_list)
         return(candidates_list)
 
-
-        #print(Counter(row[index['Candidate']] for row in csv_reader_object if row[index['Candidate']] == '').most_common())
-
-        # for row in csv_reader_object:
-        #     key = row
-        #     if key not in candidates_list:
-        #         candidates_list.extend
-        # #return candidates_list
-        # print(candidates_list)   
-        #     #candidates_list[row[2]] += 1
+def find_candidate_with_maximum_votes(path, output_file):
+    '''
+    The function takes two parameters as input - input file path and output file path and name of the file.
+    Open file in read mode.
+    Initialize rows_num - total number of rows in the csv file
+    Initialize list_of_candidates list as empty
+    Iterate through the file and add the candidate to the list of candidates, if not present yet.
+    While iteraring also count the number of rows for the percentage calculation.
+    Use max() method on the list of candidates to find the candidate with the maximum votes.
+    Using list comprehension, create a new list with candidates with the maximum votes
+    votes and calculate the percentage for the candidate with max votes.
+    '''
+    with open(path, 'r', newline='') as file_input: # open('output.txt', 'wb') as file_output:
+        csv_reader_object = csv.reader(file_input, delimiter=',')
+        next(csv_reader_object) # to skip the header
         
-        #total_profit_and_loss = round(sum(profit_list))
-        #print(f"Number of occurences for the candidate: {candidates['Khan']}.")
+        # Create a new list to store candidates from candidates column
+        rows_num = 0
+        list_of_candidates = [] 
+
+        # Start for loop to iterate through the csv file
+        for value in csv_reader_object:
+            list_of_candidates.append(value[2])
+            rows_num += 1
+            
+        #list_of_candidates_sorted
+        list_of_candidates_sorted = sorted(list_of_candidates)
+        
+        # Use Counter method to count the votes:
+        vote_count = Counter(list_of_candidates_sorted)
+        
+        # Find the maximum number of votes:
+        max_votes = max(vote_count.values())
+        
+        # Use list comprehension method to create a list with the candidates with max votes (in case votes count is the same for several candidates)
+        list_of_candidates_with_max_votes = [i for i in vote_count.keys() if vote_count[i] == max_votes]
+        
+        max_vote_percentage = ((max_votes / rows_num) * 100) # Calculate the percentage for the winner
+        print(f"{list_of_candidates_with_max_votes[0]}: {round(max_vote_percentage, 3)}% ({max_votes})")
 
         # Append to an existing file:
-        #textFileOutput = open(output_file,"a+")
-        #textFileOutput.write(f"Total $: {total_profit_and_loss} \n")
-        #textFileOutput.close()
-
-# Define a function to calculate the average change in revenue.
-'''
-The function takes path parameter as an input and perform the following:
-Open file in read mode.
-Use csv reader method to read the file line by line.
-Use next() method to skip the header.
-a)  Define two variables to store current and previous values to calculate the change.
-    Initialize both variables as None to use in the conditional statements.
-    When values are still in their initial state, then assign the current values.
-    When both values are available, then compute the difference.
-b)  For the average computation add the following:
-    Two new variables to hold the running total of changes and the row count number.
-    Calculate the average and round the results before writing to the text output file.
-c)  To identify the greatest profit increase and decrease with their respective dates, add the following:
-    Add two variables for the greatest increase and greatest decrease.
-    Add two more variables for their respective dates.
-    Track the maximum increase and maximum decrease using another IF condition inde the else branch of For loop.
-    Output the results into the summary table
-'''               
-
-def calculate_average_changes_and_pnl_max_min(path, output_file):
-    # Define the variables for the difference calculation - num0 is previous value and num1 is the current value.
-    num0 = 0 # initialize variable as None (not zero, no value assigned)
-    num1 = 0 # initialize variable as None (not zero, no value assigned)
-    runningTotalChanges = 0 # Create and initialize variable to keep the running total of the changes for future average calculation.
-    row_count = 0 # Create and initialize variable to count the total number of rows for average calculation.
-                  # Note: the row count is 86 without the header. But changes are only computed for the 85 rows, since the first row does not have previous value.
-                  # Hence, need to initialize as 0 to not take first 0 changes into the average computation.
-    changes_average = 0 # Create and initialize variable for the average changes.
-    changes_greatest_increased = 0 # Create and initialize variable for the greatest profit increase.
-    changes_greatest_decreased = 0 # Create and initialize variable for the greatest profit decrease.
-    date_greatest_increase = "" # Create and initialize variable to store the date for the greatest increase for the output file.
-    date_greatest_decrease = "" # Create and initialize variable to store the date for the greatest decrease for the output file.
-    with open(path, 'r') as file_input:
-        csv_reader_object = csv.reader(file_input, delimiter=',')
-        next(csv_reader_object) # skip the header line
-        # Start the for loop to iterate through each line in the csv file
-        for value in csv_reader_object: 
-            if num0 == 0: # Check the second row.
-                num0 = int(value[1]) 
-            else: # for all the other rows
-                num1 = int(value[1]) # assign value to the second number.
-                change = num1 - num0 # compute the difference.
-                #print(f"{num1} - {num0} = ${change}.") # print to the terminal
-                runningTotalChanges = runningTotalChanges + change # add current difference to the running total.
-                num0 = num1
-                #print(f"Running total in changes is ${runningTotalChanges}.")
-                row_count += 1
-                #print(f"Total Months: {row_count}")
-                
-                # Add condition to check if the changes are positive or negative for the Greatest increase and Greatest decrease identification.
-                if change > changes_greatest_increased:
-                    #print(f"Calculated change of ${change} larger than recorded greatest change of ${changes_greatest_increased}.")
-                    changes_greatest_increased = change
-                    date_greatest_increase = (value[0]) # get the date from the same row index[0]
-                    #print(f"The data of greatest increase is {date_greatest_increase}.")
-                    #print(f"Updated greatest change is ${change}.")
-                elif change < changes_greatest_decreased:
-                    changes_greatest_decreased = change
-                    date_greatest_decrease = (value[0]) # get the date from the same row index[0]
-                
-        # Outside of the FOR loop, compute the total of all changes over row count:
-        #print(f"Row count is = {row_count} and total changes are ${runningTotalChanges}.")
-        changes_average = runningTotalChanges / row_count # calculate the average changes.
-        print(f"Average Change: ${round(changes_average, 2)}")
-        print(f"Greatest Increase in Profits: {date_greatest_increase} (${changes_greatest_increased})")
-        print(f"Greatest Decrease in Profits: {date_greatest_decrease} (${changes_greatest_decreased})")
-
-        # Write the results to the text file:
         textFileOutput = open(output_file,"a+")
-        textFileOutput.write(f"Average Change: ${round(changes_average, 2)} \n") # round the printed output average to 2 decimal points.
-        textFileOutput.write(f"Greatest Increase in Profits: {date_greatest_increase} (${changes_greatest_increased})\n")
-        textFileOutput.write(f"Greatest Decrease in Profits: {date_greatest_decrease} (${changes_greatest_decreased})\n")
+        textFileOutput.write(f"{list_of_candidates_with_max_votes[0]}: {round(max_vote_percentage, 3)}% ({max_votes})")
         textFileOutput.close()
 
 
-# Call the functions to perform budget analysis:
+
+# Call the functions to perform elections result analysis:
+
 find_votes_total(path)
 create_candidates_list(path, output_file)
+find_candidate_with_maximum_votes(path, output_file)
+
+#test(path, output_file)
 #calculate_average_changes_and_pnl_max_min(path, output_file)
 
-print(f"The list of all the candidates: {candidates_list}.")
+#print(f"The list of all candidates: {candidates_list}.")
